bb93afbf37ad45b0ad6b1d3e6674a0c67f3bd1b2
taeshik Hwang <ghjkghj@naver.com>
2018-04-11 19:27:01 +0900

    Render Text Only Comp

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import MultipleElements from './multipleElements';
+import TextOnly from './textOnly';
 
-ReactDOM.render(<MultipleElements />, document.getElementById('root'));
+ReactDOM.render(<TextOnly />, document.getElementById('root'));
---
+++ src/textOnly.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const Comment = ({ text }) => {
+  const emojifixedText = text
+    .replace(':)', '😄')
+    .replace(':D', '😁')
+    .replace(':(', '😛');
+
+  return emojifixedText;
+};
+
+class TextOnly extends Component {
+  render() {
+    return (
+      <div>
+        <Comment text="Today we are sailing home :(" />
+      </div>
+    )
+  }
+}
+
+export default TextOnly;
