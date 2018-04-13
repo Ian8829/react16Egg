95a3135fc9689982ada9fb742b245cc779a409a2
taeshik Hwang <ghjkghj@naver.com>
2018-04-13 16:47:23 +0900

    create a DOM ref using createRef in R16.3 3

--- src/bar.js
+++ src/bar.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import BarChart from './barChart';
 
+function FunctionalComponent() {
+  let inputRef = React.createRef();
+
+  function focus() {
+    inputRef.current.focus();
+  }
+
+  return (
+    <div>
+      <input ref={inputRef} />
+      <button onClick={focus}>Focus</button>
+    </div>
+  )
+}
+
 class Bar extends Component {
-  barChartRef = React.createRef();
-
-  highlightThird = () => {
-    this.barChartRef.current.highlight(3);
-  };
-
   render() {
     return (
       <div>
-        <BarChart data={[5, 8, 14, 15, 22, 34]} ref={this.barChartRef} />
-        <button onClick={this.highlightThird}>Highlight</button>
+        <FunctionalComponent />
       </div>
     )
   }
