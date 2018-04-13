16f9c595f5f243e1089e1c529f0f8afebd903473
taeshik Hwang <ghjkghj@naver.com>
2018-04-13 11:36:38 +0900

    Render Elements Outside the current react tree using Portals in React16 2

--- src/usePortal.js
+++ src/usePortal.js
@@ -2,15 +2,46 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
+class Overlay extends Component {
+  constructor(props) {
+    super(props);
+    this.overlayContainer = document.createElement('div');
+    document.body.appendChild(this.overlayContainer);
+  }
+
+  componentWillUnmount() {
+    document.body.removeChild(this.overlayContainer);
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      <div className="overlay">
+        <span onClick={this.props.onClose}>x</span>
+        {this.props.children}
+      </div>,
+      this.overlayContainer
+    );
+  }
+}
+
 class Portal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { overlayActive: true };
+  }
+
+  closeOverlay = () => {
+    this.setState({ overlayActive: false });
+  };
+
   render() {
     return (
       <div>
         <h1>Dashboard</h1>
-        {ReactDOM.createPortal(
-          <div className="overlay">Welcome</div>,
-          document.getElementById('portal-container')
-        )}
+        {this.state.overlayActive &&
+        <Overlay onClose={this.closeOverlay}>
+          <div>Welcome</div>
+        </Overlay>}
       </div>
     );
   }
