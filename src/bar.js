dfbde2b031a230894510ae4aafce8fb32dbd9f59
taeshik Hwang <ghjkghj@naver.com>
2018-04-13 16:44:07 +0900

    Create a DOM ref using createRef in R16.3 2

--- src/bar.js
+++ src/bar.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import BarChart from './barChart';
 
 class Bar extends Component {
+  barChartRef = React.createRef();
+
+  highlightThird = () => {
+    this.barChartRef.current.highlight(3);
+  };
+
   render() {
-    return <BarChart data={[5, 8, 14, 15, 22, 34]} />;
+    return (
+      <div>
+        <BarChart data={[5, 8, 14, 15, 22, 34]} ref={this.barChartRef} />
+        <button onClick={this.highlightThird}>Highlight</button>
+      </div>
+    )
   }
 }
 
--- src/barChart.js
+++ src/barChart.js
@@ -2,16 +2,49 @@ import React, { Component, createRef } from 'react';
 import * as d3 from 'd3';
 
 class BarChart extends Component {
-  constructor(props) {
-    super(props);
-    this.chartRef = createRef();
-    console.log("constructor: ", this.chartRef.current);
-  }
+  chartRef = createRef();
 
   componentDidMount() {
-    d3.select(this.chartRef.current);
-    console.log("componentDidMount: ", this.chartRef.current);
+    const chart = d3.select(this.chartRef.current);
+    const barHeight = 20;
+    const chartWidth = 200;
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(this.props.data)])
+      .range([0, chartWidth]);
+    const bar = chart
+      .selectAll("g")
+      .data(this.props.data)
+      .enter()
+      .append("g")
+      .attr("transform", (value, i) => `translate(0,${i * barHeight})`);
+    bar
+      .append("rect")
+      .attr("width", value => xScale(value))
+      .attr("height", barHeight - 1)
+      .attr("style", "fill: steelblue;");
+    bar
+      .append("text")
+      .attr("x", value => xScale(value) - 5)
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .attr("style", "fill: white; font: 14px sans-serif; text-anchor: end;")
+      .text(value => value);
   }
+
+  highlight = index => {
+    const transition = d3
+      .transition()
+      .duration(450)
+      .ease(d3.easeLinear);
+    d3
+      .select(this.chartRef.current)
+      .select(`g:nth-child(${index}) rect`)
+      .transition(transition)
+      .style("fill", "orange")
+      .transition(transition)
+      .style("fill", "steelblue");
+  };
 
   render() {
     return <svg ref={this.chartRef} />;
